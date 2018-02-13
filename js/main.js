2124738fe49a5fb65ff32a4944924770eca4b194
Elie Ishimwe <Elie.Ishimwe@ignitiongroup.co.za>
2018-02-13 13:05:48 +0200

    Added video profile

--- js/main.js
+++ js/main.js
@@ -71,11 +71,54 @@ Vue.component('paid_videos', {
 })
 
 
-const free_video_template = { template: '<free_videos></free_videos>' }
-const paid_video_template = { template: '<paid_videos></paid_videos>' }
+Vue.component('profile_video', {
+    template: '#profile_video' ,
+    data: function () {
+
+        return {
+
+            number_of_distributors:0,
+            number_of_orders:0,
+            number_of_sims:0,
+            number_of_commissions:0,
+            last_10_orders:[]
+
+        }
+
+    },
+    methods:{
+
+        get_dashboard_data: function(){
+            const vm = this;
+            axios.get(urlBase + 'dashboardStats',getHeaders).then(function (response) {
+                vm.number_of_distributors = response.data.data.distributors;
+                vm.number_of_orders       = response.data.data.orders;
+                vm.number_of_sims         = response.data.data.sims;
+                vm.last_10_orders         = response.data.data.last_10_orders;
+
+            });
+        },
+
+    },
+    created(){
+
+        this.get_dashboard_data();
+        let recaptchaScript = document.createElement('script')
+        recaptchaScript.setAttribute('src', 'js/dashboard.js')
+        document.head.appendChild(recaptchaScript)
+
+    }
+
+})
+
+
+const free_video_template    = { template: '<free_videos></free_videos>' }
+const paid_video_template    = { template: '<paid_videos></paid_videos>' }
+const profile_video_template = { template: '<profile_video></profile_video>' }
 
 const routes = [
     { path: '/paid', component: paid_video_template},
+    { path: '/profile', component: profile_video_template},
     { path: '/', component: free_video_template }
 ];
 
