9c6b5b3f6a7f086b4a509cfcca69ab4764493bf2
Elie Ishimwe <Elie.Ishimwe@ignitiongroup.co.za>
2018-02-19 14:04:52 +0200

    Added holdon js

--- admin/assets/js/backend.js
+++ admin/assets/js/backend.js
@@ -4,31 +4,38 @@ $(document).ready(function(){
 
     var formData = {"member_id" : 12};
 
+    if($('#videoDatatable').length){
 
-    var oTable     = $('#videoDatatable').DataTable({
-        "dom": 'T<"clear">lfrtip',
-        "ajax": {
-            "url": "http://ec2-52-200-186-135.compute-1.amazonaws.com/api_twominutes/index.php/api/get_user_videos",
-            "contentType": "application/json",
-            "type": "POST",
-            "dataSrc":"",
-            "data": function (formData) {
-                formData = {"member_id" : 12};
-                return JSON.stringify( formData );
-            }
+        var oTable     = $('#videoDatatable').DataTable({
+            "dom": 'T<"clear">lfrtip',
+            "ajax": {
+                "url": "http://ec2-52-200-186-135.compute-1.amazonaws.com/api_twominutes/index.php/api/get_user_videos",
+                "contentType": "application/json",
+                "type": "POST",
+                "dataSrc":"",
+                "data": function (formData) {
+                    formData = {"member_id" : 12};
+                    return JSON.stringify( formData );
+                }
 
-        },
-        "columns": [
-            {data: 'videos_id'},
-            {data: 'date_created'},
-            {data: 'videos_title'},
-            {data: 'videos_path'},
-
-
-        ]
+            },
+            "columns": [
+                {data: 'videos_id'},
+                {data: 'date_created'},
+                {data: 'videos_title'},
+                {data: 'videos_path'},
 
 
-    });
+            ]
+
+
+        });
+
+
+
+    }
+
+
 
 
 });
@@ -39,14 +46,26 @@ $("#submitvideo").submit(function(e) {
     e.preventDefault();
     var formData = new FormData(this);
 
-    console.log(formData);
 
     $.ajax({
         url: 'http://ec2-52-200-186-135.compute-1.amazonaws.com/api_twominutes/index.php/api/upload_video/',
         type: 'POST',
         data: formData,
+        beforeSend : function() {
+
+            HoldOn.open({
+                theme:"sk-bounce",//If not given or inexistent theme throws default theme sk-rect
+                message: "<h4> Downloading ......</h4>",
+                content:"Your HTML Content", // If theme is set to "custom", this property is available
+                                             // this will replace the theme by something customized.
+                backgroundColor:"blue",//Change the background color of holdon with javascript
+                                       // Keep in mind is necessary the .css file too.
+                textColor:"white" // Change the font color of the message
+            });
+
+        },
         success: function (data) {
-            alert(data)
+            HoldOn.close();
         },
         cache: false,
         contentType: false,
